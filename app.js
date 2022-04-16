707a47b964ade76e9f323b4b70bf8e814657c6b7
Deon Butler <deonbutler44@gmail.com>
2022-04-15 15:04:54 -1000

    Clean up project. Implemented mongoose

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
-mongoose.connect("mongodb://localhost/demo-express");
-let db = mongoose.connection;
+mongoose.connect('mongodb://127.0.0.1/demo-express', function(err, db){
+    if (err) {
+        console.log('Unable to connect to server. Please check server connection. Error:', err);
+    } else {
+        console.log('Connected to MongoDB');
+    }
+});
+
+/*let db = mongoose.connection;
+
 
 // Check connection
 db.once('open', function(){
@@ -13,48 +22,60 @@ db.once('open', function(){
 // Check for db errors
 db.on('error', function(err){
     console.log(err);
-})
+})*/
 
 // Init App
 const app = express();
+
+// Bring in models
+let Article = require('./models/article');
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false}))
+//parse application/json
+app.use(bodyParser.json())
+
 // Home Route
 app.get('/', function(req, res){
-    let articles = [
-        {
-            id:1,
-            title: 'Article One',
-            author: 'Deon Butler',
-            body: 'This is article one'
-        },
-        {
-            id:2,
-            title: 'Article Two',
-            author: 'Deon Butler',
-            body: 'This is article two'
-        },
-        {
-            id:3,
-            title: 'Article Three',
-            author: 'Deon Butler',
-            body: 'This is article three'
+    Article.find({}, function (err, articles){
+        if (err){
+            console.log(err);
+        } else {
+            res.render('index', {
+                title: 'Articles',
+                articles: articles
+            });
         }
-    ];
-    res.render('index', {
-        title:'Articles',
-        articles: articles
     });
 });
+
 
 // Add Route
 app.get('/article/add', function(req, res){
     res.render('add_article', {
         title: 'Add Article'
     });
+});
+
+// Add Submit POST Route
+app.post('/article/add', function(req, res){
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save(function(err){
+        if(err){
+            console.log('There was an error', err);
+        } else {
+            res.redirect('/');
+        }
+    })
 });
 
 // Start Server
--- models/article.js
+++ models/article.js
@@ -5,11 +5,11 @@ let articleSchema = mongoose.Schema({
     title:{
         type: String,
         required:true
-    }
+    },
     author:{
         type: String,
         required:true
-    }
+    },
     body:{
         type: String,
         required:true
